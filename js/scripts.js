4669fc13ef385bb6f46649be3ab636054e614f63
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-09 21:05:22 -0600

    add JS for first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-  $
+//Business Logic
+var leapYear = function(year) {
+  
+    return false;
+  
+};
 
-  event.preventDefault();
+
+//User Interface Logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  })
+
 })
